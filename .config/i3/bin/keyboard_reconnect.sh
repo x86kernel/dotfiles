3d41f5740a6ea32ab0c142eb69721498eaff1cc6
Jae Young Mun <x86kernel@gmail.com>
2022-02-19 05:28:36 +0900

    feat(.config, .xinitrc, .Xresources): update files

--- .config/i3/bin/keyboard_reconnect.sh
+++ .config/i3/bin/keyboard_reconnect.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-if [ "$CONNECT_TYPE" == "reconnect" ]
-then
-    export DISPLAY=:0
-    export XAUTHORITY=/home/x86kernel/.Xauthority
-fi
+export DISPLAY=:0
+export XAUTHORITY=/home/x86kernel/.Xauthority
 
 # for Notebook only
 # xmodmap -e 'remove mod1 = Alt_R'
 # xmodmap -e "remove control = Control_R"
 # xmodmap -e "keycode 105=Hangul_Hanja"
-xmodmap -e "keycode 134=Hangul" # for hhkb
+# xmodmap -e "keycode 134=Hangul" # for Notebook
 ###################
 
 # for Keychron K6 only
-#xmodmap -e "keycode 106=Hangul" # for Notebook
+xmodmap -e "keycode 108=Hangul" # for Notebook
 ######################
